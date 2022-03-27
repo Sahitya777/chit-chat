6ad7558d4b14a992944f4e50d21a7f668feb6e7c
Sahitya77 <sahityanijhawan@gmail.com>
2022-03-27 15:17:03 +0530

    Chit chat app!!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import {ChatEngine} from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="ef818dce-ef3d-452b-bcf4-fd7ea25902bd"
+      userName="Sahitya Nijhawan"
+      userSecret="12345"
+      renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps}/>}
+    />
   );
 }
 
