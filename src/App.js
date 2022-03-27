6048cc6db87244b899225376866d2746b1e8e5ba
Sahitya77 <sahityanijhawan@gmail.com>
2022-03-27 16:07:39 +0530

    login updated!!

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import {ChatEngine} from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 function App() {
+
+  if(!localStorage.getItem('username')) return <LoginForm/>
+
   return (
     <ChatEngine
       height="100vh"
       projectID="ef818dce-ef3d-452b-bcf4-fd7ea25902bd"
-      userName="Sahitya Nijhawan"
-      userSecret="12345"
+      userName="badbiker"
+      userSecret="123123"
       renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps}/>}
     />
   );
